6bb8513c17b29f952a007529fd8a34cd1a3b5c84
guptaGirish <girish.g@srijan.in>
2013-07-26 11:10:22 +0530

    bkjb

--- src/com/example/trex/SettingsActivity.java
+++
@@ -1,5 +0,0 @@
-package com.example.trex;
-
-public class SettingsActivity {
-
-}
--- src/com/example/trex/adapters/CategoryDbAdapter.java
+++
@@ -1,7 +0,0 @@
-package com.example.trex.adapters;
-
-public class CategoryDbAdapter {
-	
-	static String NAME = "cname" ;
-
-}
